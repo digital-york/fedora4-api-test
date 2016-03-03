4adaa5e90bfb25c370e127cfd7c39cab16dfb410
Frank Feng <frank.feng@york.ac.uk>
2016-03-03 14:43:26 +0000

    REST GET requests

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fedora4-api-test
+To run scripts, first create a property file: /home/vagrant/system.properties, and put Fedora credentials:
+{
+   :username  => 'USERNAME',
+   :password  => 'PASSWORD'
+}
---
+++ testGet1.rb
@@ -0,0 +1,21 @@
+require 'faraday'
+
+props = eval(File.open('/home/vagrant/system.properties') {|f| f.read})
+username = props[:username]
+password = props[:password]
+
+conn = Faraday.new(:url => 'http://localhost:8080') do |c|
+  c.use Faraday::Request::UrlEncoded  # encode request params as "www-form-urlencoded"
+  c.use Faraday::Response::Logger     # log request & response to STDOUT
+  c.use Faraday::Adapter::NetHttp     # perform requests with Net::HTTP
+end
+conn.basic_auth(username, password)
+
+#By default, Fedora returns turtle
+response = conn.get '/fcrepo/rest/test' 
+puts '---------Text/Turtle response-----------'
+puts response.body.inspect
+
+response = conn.get '/fcrepo/rest/test', {}, {'Accept' => 'application/rdf+xml'}
+puts '---------XML response-----------'
+puts response.body.inspect
